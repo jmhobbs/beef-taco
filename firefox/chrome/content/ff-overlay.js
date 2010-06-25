32541f7ee882b140f5552eb3ee7f446367d80448
John Hobbs <john@velvetcache.org>
2010-06-25 17:55:32 -0500

    Added a menu option shortcut to the show all cookies dialog.

--- firefox/chrome/content/ff-overlay.js
+++ firefox/chrome/content/ff-overlay.js
@@ -1,4 +1,7 @@
 com.github.jmhobbs.beef_taco.onFirefoxLoad = function ( event ) {
+	this.initialized = true;
+	this.strings = document.getElementById( "beef-taco-strings" );
+	
 	document.getElementById( "contentAreaContextMenu" ).addEventListener(
 		"popupshowing",
 		function ( e ) {
@@ -10,6 +13,14 @@ com.github.jmhobbs.beef_taco.onFirefoxLoad = function ( event ) {
 
 com.github.jmhobbs.beef_taco.showFirefoxContextMenu = function ( event ) {
 	document.getElementById( "context-beef-taco" ).hidden = gContextMenu.onImage;
+};
+
+com.github.jmhobbs.beef_taco.showAllCookies = function( event ) {
+	window.openDialog(
+		"chrome://browser/content/preferences/cookies.xul",
+		"_blank",
+		"chrome,resizable=yes"
+	);
 };
 
 window.addEventListener( "load", com.github.jmhobbs.beef_taco.onFirefoxLoad, false );
