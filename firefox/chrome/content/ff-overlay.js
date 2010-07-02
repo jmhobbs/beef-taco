d6248cb02c809e6f7da53bb638d016e4d2919c93
John Hobbs <john@velvetcache.org>
2010-07-02 17:53:19 -0500

    Got the toolbar preferences working.

--- firefox/chrome/content/ff-overlay.js
+++ firefox/chrome/content/ff-overlay.js
@@ -2,29 +2,29 @@ com.github.jmhobbs.beef_taco.onStartup = function ( event ) {
 	this.initialized = true;
 	this.strings = document.getElementById( "beef-taco-strings" );
 	
-	this.prefs = Components.classes["@mozilla.org/preferences-service;1"]
+	this.preferences = Components.classes["@mozilla.org/preferences-service;1"]
 			.getService( Components.interfaces.nsIPrefService )
 			.getBranch( "extensions.beef-taco." );
-	this.prefs.QueryInterface( Components.interfaces.nsIPrefBranch2 );
+	this.preferences.QueryInterface( Components.interfaces.nsIPrefBranch2 );
 	
-	this.cookie_toolbar = document.getElementById( "beef-taco-cookies" );
-	this.cookie_toolbar.hidden = ! this.prefs.getBoolPref( "showtoolbarpref" );
+	document.getElementById( "beef-taco-cookies" ).hidden = ! this.preferences.getBoolPref( "showtoolbarpref" );
 	
-	this.prefs.addObserver( "", com.github.jmhobbs.beef_taco, false );
+	this.preferences.addObserver( "", com.github.jmhobbs.beef_taco.PreferencesObserver, false );
 };
 
 com.github.jmhobbs.beef_taco.onShutdown = function() {
-	this.prefs.removeObserver( "", this );
+	this.preferences.removeObserver( "", com.github.jmhobbs.beef_taco.PreferencesObserver );
 };
 
-com.github.jmhobbs.beef_taco.observe = function ( subject, topic, data ) {
-	if( topic != "nsPref:changed" ) { return; }
-	switch ( data ) {
-		case "showtoolbarpref":
-// 			alert( 'In!' );
-// 			alert( com.github.jmhobbs.beef_taco.prefs );
-			//this.cookie_toolbar.hidden = ! this.prefs.getBoolPref( "showtoolbarpref" );
-			break;
+com.github.jmhobbs.beef_taco.PreferencesObserver = {
+	observe: function( subject, topic, data ) {
+		if( topic != "nsPref:changed" ) { return; }
+		switch ( data ) {
+			case "showtoolbarpref":
+				// This isn't the best way in the world, but hey, it's a boolean, it should work, right?
+				document.getElementById( "beef-taco-cookies" ).hidden = ! document.getElementById( "beef-taco-cookies" ).hidden;
+				break;
+		}
 	}
 };
 
