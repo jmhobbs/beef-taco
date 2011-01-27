2ea88bc6c43ce5e0f41785d9f804861f866b807a
John Hobbs <john@velvetcache.org>
2011-01-26 23:00:12 -0600

    Added the cookies that Jim B. from PrivacyChoice sent me.

--- firefox/chrome/content/cookies.js
+++ firefox/chrome/content/cookies.js
@@ -1,4 +1,4 @@
-// Generated: Fri, 08 Oct 2010 15:51:47 +0000
+// Generated: Thu, 27 Jan 2011 04:56:44 +0000
 com.github.jmhobbs.beef_taco.cookies = {
 	// [X+1]
 	".edge.ru4.com" :  [ { path: "/", name: "ru4.optout", value: "true" } ],
@@ -29,15 +29,15 @@ com.github.jmhobbs.beef_taco.cookies = {
 	// Adshuffle
 	".adshuffle.com" :  [ { path: "/", name: "OptedOut", value: "1" } ],
 	// Adtech
-	".adtech.de" :  [ { path: "/", name: "OptOut", value: "we will not set any more cookies" } ],
-	// Adtech
-	".glb.adtechus.com" :  [ { path: "/", name: "OptOut", value: "we will not set any more cookies" } ],
+	".ad.us-ec.adtechus.com" :  [ { path: "/", name: "OptOut", value: "we will not set any more cookies" } ],
 	// Adtech
 	".adserverec.adtechus.com" :  [ { path: "/", name: "OptOut", value: "we will not set any more cookies" } ],
 	// Adtech
 	".adserverwc.adtechus.com" :  [ { path: "/", name: "OptOut", value: "we will not set any more cookies" } ],
 	// Adtech
-	".ad.us-ec.adtechus.com" :  [ { path: "/", name: "OptOut", value: "we will not set any more cookies" } ],
+	".adtech.de" :  [ { path: "/", name: "OptOut", value: "we will not set any more cookies" } ],
+	// Adtech
+	".glb.adtechus.com" :  [ { path: "/", name: "OptOut", value: "we will not set any more cookies" } ],
 	// Advertising.com / AOL
 	".advertising.com" :  [ { path: "/", name: "ACID", value: "optout!" } ],
 	// Aggregate Knowledge
@@ -86,18 +86,18 @@ com.github.jmhobbs.beef_taco.cookies = {
 	".adcentriconline.com" :  [ { path: "/", name: "AdcentricOptout", value: "true" } ],
 	// Criteo
 	".criteo.com" :  [ { path: "/", name: "optout", value: "1" } ],
-	// Dotomi
-	".dotomi.com" :  [ { path: "/", name: "DotomiStatus", value: "5" } ],
 	// DataXu
 	".w55c.net" :  [ { path: "/", name: "optout", value: "1" } ],
-	// E-planning
-	".ads.us.e-planning.net" :  [ { path: "/", name: "US.E-Planning.NET", value: "OPTOUT" } ],
+	// Dotomi
+	".dotomi.com" :  [ { path: "/", name: "DotomiStatus", value: "5" } ],
 	// E-planning
 	".ads.e-planning.net" :  [ { path: "/", name: "E-Planning.NET", value: "OPTOUT" } ],
-	// Effective Measure
-	".effectivemeasure.net" :  [ { path: "/", name: "_em_opt_out", value: "1" } ],
+	// E-planning
+	".ads.us.e-planning.net" :  [ { path: "/", name: "US.E-Planning.NET", value: "OPTOUT" } ],
 	// Effective Measure
 	".effectivemeasure.com" :  [ { path: "/", name: "_em_opt_out", value: "1" } ],
+	// Effective Measure
+	".effectivemeasure.net" :  [ { path: "/", name: "_em_opt_out", value: "1" } ],
 	// Eloqua
 	".eloqua.com" :  [ { path: "/", name: "ELOQUA", value: "GUID=00000000000000000000000000000000" } ],
 	// Exelator
@@ -107,25 +107,25 @@ com.github.jmhobbs.beef_taco.cookies = {
 	// Exelator
 	".loadan.exelator.net" :  [ { path: "/load/", name: "DNP", value: "eXelate+OptOut" } ],
 	// Exelator
-	".loadxl.exelator.net" :  [ { path: "/load/", name: "DNP", value: "eXelate+OptOut" } ],
-	// Exelator
-	".loadxl.exelator.com" :  [ { path: "/load/", name: "DNP", value: "eXelate+OptOut" } ],
-	// Exelator
-	".loadxl.exelator.com" :  [ { path: "/load/", name: "DNP", value: "eXelate+OptOut" } ],
-	// Exelator
 	".loadxl.exelator.biz" :  [ { path: "/load/", name: "DNP", value: "eXelate+OptOut" } ],
+	// Exelator
+	".loadxl.exelator.com" :  [ { path: "/load/", name: "DNP", value: "eXelate+OptOut" } ],
+	// Exelator
+	".loadxl.exelator.com" :  [ { path: "/load/", name: "DNP", value: "eXelate+OptOut" } ],
+	// Exelator
+	".loadxl.exelator.net" :  [ { path: "/load/", name: "DNP", value: "eXelate+OptOut" } ],
 	// Eyeblaster /  serving-sys.com
 	".serving-sys.com" :  [ { path: "/", name: "ebOptOut", value: "TRUE" } ],
 	// EyeWonder
 	".eyewonder.com" :  [ { path: "/", name: "ew-optout", value: "1" } ],
-	// Facilitate Digital
-	".adsfac.us" :  [ { path: "/", name: "OO", value: "OO=1" } ],
 	// Facilitate Digital
 	".adsfac.eu" :  [ { path: "/", name: "OO", value: "OO=1" } ],
 	// Facilitate Digital
 	".adsfac.net" :  [ { path: "/", name: "OO", value: "OO=1" } ],
 	// Facilitate Digital
 	".adsfac.sg" :  [ { path: "/", name: "OO", value: "OO=1" } ],
+	// Facilitate Digital
+	".adsfac.us" :  [ { path: "/", name: "OO", value: "OO=1" } ],
 	// fastclick (mediaplex/valueclick)
 	".fastclick.net" :  [ { path: "/", name: "fastclick", value: "optout" } ],
 	// Fetchback
@@ -159,9 +159,9 @@ com.github.jmhobbs.beef_taco.cookies = {
 	// Microsoft / Live.com
 	".live.com" :  [ { path: "/", name: "TOptOut", value: "1" } ],
 	// Microsoft Advertising
-	".msn.com" :  [ { path: "/", name: "TOptOut", value: "1" } ],
-	// Microsoft Advertising
 	".bing.com" :  [ { path: "/", name: "TOptOut", value: "1" } ],
+	// Microsoft Advertising
+	".msn.com" :  [ { path: "/", name: "TOptOut", value: "1" } ],
 	// Mindset Media / mmismm.com
 	".mmismm.com" :  [ { path: "/", name: "G", value: "-1" } ],
 	// Navegg
@@ -219,11 +219,11 @@ com.github.jmhobbs.beef_taco.cookies = {
 	// Snoobi
 	".snoobi.com" :  [ { path: "/", name: "Snoobi_optout", value: "1" } ],
 	// Specific Media
+	".adviva.net" :  [ { path: "/", name: "ADVIVA", value: "NOTRACK" } ],
+	// Specific Media
 	".specificclick.net" :  [ { path: "/", name: "ADVIVA", value: "NOTRACK" } ],
 	// Specific Media
 	".specificmedia.com" :  [ { path: "/", name: "ADVIVA", value: "NOTRACK" } ],
-	// Specific Media
-	".adviva.net" :  [ { path: "/", name: "ADVIVA", value: "NOTRACK" } ],
 	// Tacoda
 	".tacoda.net" :  [ { path: "/", name: "TID", value: "tacodaamoptout" } ],
 	// Tatto Media
@@ -264,4 +264,256 @@ com.github.jmhobbs.beef_taco.cookies = {
 	".yumenetworks.com" :  [ { path: "/", name: "ymoo", value: "true" } ],
 	// Zedo (Undertone Networks)
 	".zedo.com" :  [ { path: "/", name: "ZEDOIDA", value: "OPT_OUT" } ],
+	// 
+	".adchemy.com" :  [ { path: "/", name: "adc_optout", value: "opted_out" } ],
+	// 
+	".addthis.com" :  [ { path: "/", name: "uid", value: "0" } ],
+	// 
+	".adgear.com" :  [ { path: "/", name: "AdGear_OPTOUT", value: "agOptedOut" } ],
+	// 
+	".adnxs.com" :  [ { path: "/", name: "uuid2", value: "-1" } ],
+	// 
+	".adroll.com" :  [ { path: "/", name: "__adroll_opt_out", value: "OPT_OUT_foobar" } ],
+	// 
+	".adsummos.net" :  [ { path: "/", name: "adsud0", value: "OPT-OUT" } ],
+	// 
+	".amazon.com" :  [ { path: "/", name: "apn-privacy", value: "1" } ],
+	// 
+	".aol.co.uk" :  [ { path: "/", name: "WlOptOut", value: "Yes" } ],
+	// 
+	".ask.com" :  [ { path: "/", name: "askeraser", value: "on" } ],
+	// 
+	".atdmt.com" :  [ { path: "/", name: "ID", value: "optout" } ],
+	// 
+	".beencounter.com" :  [ { path: "/", name: "cookie3", value: "no-track" } ],
+	// 
+	".brand.net" :  [ { path: "/", name: "OPT", value: "out" } ],
+	// 
+	".buzzlogic.com" :  [ { path: "/", name: "BL_Opted_Out", value: "1" } ],
+	// 
+	".chango.com" :  [ { path: "/", name: "chango.optout", value: "1" } ],
+	// 
+	".chitika.net" :  [ { path: "/", name: "AO", value: "o%3D1" } ],
+	// 
+	".constantcontact.com" :  [ { path: "/", name: "opt-out", value: "opt-out" } ],
+	// 
+	".contextweb.com" :  [ { path: "/", name: "CWOptOutCookie", value: "1" } ],
+	// 
+	".convertro.com" :  [ { path: "/", name: "cvo_sid1", value: "100000000000" } ],
+	// 
+	".ctasnet.com" :  [ { path: "/", name: "o", value: "9" } ],
+	// 
+	".demdex.net" :  [ { path: "/", name: "demdex", value: "NOTARGET" } ],
+	// 
+	".displaymarketplace.com" :  [ { path: "/", name: "NoTrack", value: "6.34086120591247E+017" } ],
+	// 
+	".dotomi.com" :  [ { path: "/", name: "DotomiRR2000", value: "-1$1$1$" } ],
+	// 
+	".esm1.net" :  [ { path: "/", name: "optout", value: "yes" } ],
+	// 
+	".everesttech.net" :  [ { path: "/", name: "ev_optout", value: "1" } ],
+	// 
+	".exelator.com" :  [ { path: "/", name: "DNP", value: "eXelate+OptOut" } ],
+	// 
+	".eyereturn.com" :  [ { path: "/", name: "EROO", value: "O" } ],
+	// 
+	".gbid.adbuyer.com" :  [ { path: "/", name: "tuuid", value: "opt-out" } ],
+	// 
+	".gigya.com" :  [ { path: "/", name: "GigNoCK", value: "1" } ],
+	// 
+	".gwallet.com" :  [ { path: "/", name: "ra1_oo", value: "1" } ],
+	// 
+	".hitbox.com" :  [ { path: "/", name: "WSS_GW", value: "V1z%BX^e@Ceer" } ],
+	// 
+	".impressiondesk.com" :  [ { path: "/", name: "optout", value: "*" } ],
+	// 
+	".keewurd.com" :  [ { path: "/", name: "OptOut", value: "1" } ],
+	// 
+	".legolas-media.com" :  [ { path: "/", name: "optout", value: "1" } ],
+	// 
+	".lijit.com" :  [ { path: "/", name: "tracking_optout", value: "1" } ],
+	// 
+	".meebo.com" :  [ { path: "/", name: "tcookie", value: "OPT_OUT" } ],
+	// 
+	".monster.com" :  [ { path: "/", name: "CANOptOut", value: "1" } ],
+	// 
+	".mookie1.com" :  [ { path: "/", name: "optouts", value: "cookies" } ],
+	// 
+	".mxptint.net" :  [ { path: "/", name: "mxpim", value: "optout" } ],
+	// 
+	".mybuys.com" :  [ { path: "/", name: "mbc", value: "optout" } ],
+	// 
+	".mythings.com" :  [ { path: "/", name: "optout", value: "1" } ],
+	// 
+	".newtention.net" :  [ { path: "/", name: "id", value: "optout" } ],
+	// 
+	".owneriq.net" :  [ { path: "/", name: "optout", value: "optout" } ],
+	// 
+	".predictad.com" :  [ { path: "/", name: "predictad_dc", value: "1" } ],
+	// 
+	".proximic.com" :  [ { path: "/", name: "optout", value: "1" } ],
+	// 
+	".pulsemgr.com" :  [ { path: "/", name: "x", value: "OPTOUT" } ],
+	// 
+	".qnsr.com" :  [ { path: "/", name: "QIDA", value: "OPT_OUT" } ],
+	// 
+	".raasnet.com" :  [ { path: "/", name: "o", value: "9" } ],
+	// 
+	".raasnet.com" :  [ { path: "/", name: "ctrn", value: "0" } ],
+	// 
+	".rfihub.com" :  [ { path: "/", name: "k", value: "bOPT-OUT" } ],
+	// 
+	".rubiconproject.com" :  [ { path: "/", name: "fu", value: "on" } ],
+	// 
+	".saymedia.com" :  [ { path: "/", name: "UsesLocalStoredObject", value: "NotDetermined" } ],
+	// 
+	".scorecardresearch.com" :  [ { path: "/", name: "NO_COOKIE", value: "1" } ],
+	// 
+	".server.cpmstar.com" :  [ { path: "/", name: "x", value: "1" } ],
+	// 
+	".sharethis.com" :  [ { path: "/", name: "st_optout", value: "1" } ],
+	// 
+	".simpli.fi" :  [ { path: "/", name: "opted_out", value: "1" } ],
+	// 
+	".spongecell.com" :  [ { path: "/", name: "auid", value: "0" } ],
+	// 
+	".spongecell.com" :  [ { path: "/", name: "_spongecell_loves_u", value: "" } ],
+	// 
+	".struq.com" :  [ { path: "/", name: "pref", value: "optout" } ],
+	// 
+	".svc.pch.com" :  [ { path: "/", name: "Optout", value: "OPT_OUT" } ],
+	// 
+	".tag.admeld.com" :  [ { path: "/", name: "admeld_opt_out", value: "1" } ],
+	// 
+	".tellapart.com" :  [ { path: "/", name: "btexclude", value: "1" } ],
+	// 
+	".teracent.net" :  [ { path: "/", name: "uid", value: "opt_out.###" } ],
+	// 
+	".tidaltv.com" :  [ { path: "/", name: "opt-out", value: "1" } ],
+	// 
+	".tradedoubler.com" :  [ { path: "/", name: "OPTOUT", value: "z11zzujzlRSz2" } ],
+	// 
+	".traveladvertising.com" :  [ { path: "/", name: "PRIVACY", value: "1" } ],
+	// 
+	".triggit.com" :  [ { path: "/", name: "trgoptout", value: "1" } ],
+	// 
+	".valueclick.net" :  [ { path: "/", name: "ksa", value: "optout" } ],
+	// 
+	".veruta.com" :  [ { path: "/", name: "optout", value: "1" } ],
+	// 
+	".vindicosuite.com" :  [ { path: "/", name: "VINDICOAUDIENCEISSUEDIDENTITY", value: "VINDICOAUDIENCEOPTOUT" } ],
+	// 
+	".visiblemeasures.com" :  [ { path: "/", name: "UsesLocalStoredObject", value: "NotDetermined" } ],
+	// 
+	".wtp101.com" :  [ { path: "/", name: "tuuid", value: "opt-out" } ],
+	// 
+	"ad.wsod.com" :  [ { path: "/", name: "u_1", value: "OPT_OUT" } ],
+	// 
+	"ad.yieldmanager.com" :  [ { path: "/", name: "optout", value: "1" } ],
+	// 
+	"ad.yieldmanager.com" :  [ { path: "/", name: "optout", value: "1" } ],
+	// 
+	"ad.yieldmanager.com" :  [ { path: "/", name: "optout", value: "1" } ],
+	// 
+	"adadvisor.net" :  [ { path: "/", name: "id", value: "opt-out" } ],
+	// 
+	"admonkey.dapper.net" :  [ { path: "/", name: "DAPPEROPTOUT", value: "OPT-OUT" } ],
+	// 
+	"ads.bridgetrack.com" :  [ { path: "/", name: "BTA", value: "GUID=00000000000000000000000000000000" } ],
+	// 
+	"ads.e-planning.net" :  [ { path: "/", name: "E-Planning.NET", value: "OPTOUT" } ],
+	// 
+	"ads.heias.de" :  [ { path: "/", name: "hco", value: "a%3A2%3A%7Bi%3A0%3Bs%3A4%3A%22TRUE%22%3Bi%3A1%3Bi%3A1397354654%3B%7D" } ],
+	// 
+	"ads.pointroll.com" :  [ { path: "/", name: "PRID", value: "OPTOUT" } ],
+	// 
+	"ads.undertone.com" :  [ { path: "/", name: "UTOPTOUT", value: "OPTOUT" } ],
+	// 
+	"ads.us.e-planning.net" :  [ { path: "/", name: "US.E-Planning.NET", value: "OPTOUT" } ],
+	// 
+	"adsfac.eu" :  [ { path: "/", name: "OO", value: "OO=1" } ],
+	// 
+	"adsfac.net" :  [ { path: "/", name: "OO", value: "OO=1" } ],
+	// 
+	"adsfac.sg" :  [ { path: "/", name: "OO", value: "OO=1" } ],
+	// 
+	"adsfac.us" :  [ { path: "/", name: "OO", value: "OO=1" } ],
+	// 
+	"adsonar.com" :  [ { path: "/", name: "oo_flag", value: "f" } ],
+	// 
+	"adv.adsbwm.com" :  [ { path: "/", name: "OxaMedia_OPT_OUT", value: "" } ],
+	// 
+	"amadesa.com" :  [ { path: "/", name: "amOptout", value: "1" } ],
+	// 
+	"amgdgt.com" :  [ { path: "/", name: "OptOut", value: "AAAAAQAUMcmt2dLy01_5OEIR8ei6TnoK9XEAAEwUvPxVWUKxtPOEtjKZGR8-" } ],
+	// 
+	"apps.pubmatic.com" :  [ { path: "/", name: "pmoo", value: "1" } ],
+	// 
+	"assets.invitemedia.com" :  [ { path: "/", name: "optout", value: "1" } ],
+	// 
+	"assets.invitemedia.com" :  [ { path: "/", name: "uid", value: "0" } ],
+	// 
+	"bluestreak.com" :  [ { path: "/", name: "id", value: "OPT_OUT" } ],
+	// 
+	"bnmla.com" :  [ { path: "/", name: "optout", value: "1" } ],
+	// 
+	"connect.wunderloop.net" :  [ { path: "/", name: "WlOptOut", value: "DONT" } ],
+	// 
+	"crwdcntrl.net" :  [ { path: "/", name: "cc", value: "optout" } ],
+	// 
+	"designbloxlive.com" :  [ { path: "/", name: "Adroit_id", value: "opt_out" } ],
+	// 
+	"designbloxlive.com" :  [ { path: "/", name: "Adroit_vars", value: "opt_out" } ],
+	// 
+	"domdex.com" :  [ { path: "/", name: "optout", value: "1" } ],
+	// 
+	"doubleclick.net" :  [ { path: "/", name: "id", value: "OPT_OUT" } ],
+	// 
+	"fwmrm.net" :  [ { path: "/", name: "opt_out", value: "1" } ],
+	// 
+	"g.adspeed.net" :  [ { path: "/", name: "ASOptOut", value: "1" } ],
+	// 
+	"interclick.com" :  [ { path: "/", name: "Opt", value: "out" } ],
+	// 
+	"load.exelator.com" :  [ { path: "/", name: "DNP", value: "eXelate+OptOut" } ],
+	// 
+	"log.doubleverify.com" :  [ { path: "/", name: "dv_opt_out", value: "0" } ],
+	// 
+	"lulu.ru4.com" :  [ { path: "/", name: "ru4.optout", value: "1" } ],
+	// 
+	"nextag.com" :  [ { path: "/", name: "ntad", value: "opt-out" } ],
+	// 
+	"nspmotion.com" :  [ { path: "/", name: "admcol", value: "ip=1200750637&dbs=0&fs=0&uid=1" } ],
+	// 
+	"opt.w55c.net" :  [ { path: "/", name: "optout", value: "1" } ],
+	// 
+	"optout.ib-ibi.com" :  [ { path: "/", name: "ib-ibi.com-OptOut", value: "IsOptOut=true" } ],
+	// 
+	"p.brilig.com" :  [ { path: "/", name: "BriligContact", value: "OPT_OUT" } ],
+	// 
+	"qjex.net" :  [ { path: "/", name: "optout", value: "1" } ],
+	// 
+	"rlcdn.com" :  [ { path: "/", name: "optout", value: "Opted+Out+of+RapLeaf+Cookies%2C+to+re-enable+simply+delete+this+cookie." } ],
+	// 
+	"search.netseer.com" :  [ { path: "/", name: "netseer_optout", value: "optout" } ],
+	// 
+	"spongecell.com" :  [ { path: "/", name: "cookie_opt_out", value: "1" } ],
+	// 
+	"spotexchange.com" :  [ { path: "/", name: "opt_out-0", value: "b3B0X291dAlvcHRfb3V0CWNvb2tpZV9kb21haW4Jd3d3LnNwb3R4Y2hhbmdlLmNvbQljcmVhdGVkX2RhdGUJMTI0MDAwMzQ5Nwltb2RpZmllZF9kYXRlCTEyNDAwMDM0OTcK" } ],
+	// 
+	"tealium.com" :  [ { path: "/", name: "SM_OPTOUT", value: "1" } ],
+	// 
+	"tracking.intermundomedia.com" :  [ { path: "/", name: "ID", value: "OPT_OUT" } ],
+	// 
+	"tracking.quisma.com" :  [ { path: "/", name: "tracker_optout", value: "1" } ],
+	// 
+	"tracking.reedge.com" :  [ { path: "/", name: "REED_optout", value: "all%3DMQ%3D%3D" } ],
+	// 
+	"tribalfusion.com" :  [ { path: "/", name: "ANON_ID", value: "OptOut" } ],
+	// 
+	"vitamine.networldmedia.net" :  [ { path: "/", name: "OPTOUT", value: "1" } ],
+	// 
+	"www.acxiom.com" :  [ { path: "/", name: "CP", value: "null*" } ],
+	// 
+	"www.inadcoads.com" :  [ { path: "/", name: "_iad_vsid", value: "99999999-9999-9999-9999-999999999999" } ],
 }
